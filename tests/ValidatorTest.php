535db691527c21e1ee55251a9065e0c54d0b7b4b
Pereyaslov Konstantin <perk11@perk11.info>
2014-12-08 21:59:31 +0300

    Validator test

---
+++ tests/ValidatorTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace pdt256\Shipping;
+
+class ValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \LogicException
+     */
+    public function testNull()
+    {
+        Validator::checkIfNull(null, 'null');
+    }
+
+    public function testNotNull()
+    {
+        Validator::checkIfNull('XXX', 'notNullValue');
+        Validator::checkIfNull([], 'notNullValue');
+        Validator::checkIfNull(new \stdClass(), 'notNullValue');
+        Validator::checkIfNull(function() {
+        }, 'notNullValue');
+    }
+}
