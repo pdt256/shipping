41bc29564c3aa5f1e3fb740e5bff913d39efa617
Hari KT <kthari85@gmail.com>
2019-12-12 22:56:36 +0530

    update timeout

--- src/RateRequest/Adapter.php
+++ src/RateRequest/Adapter.php
@@ -3,8 +3,8 @@ namespace pdt256\Shipping\RateRequest;
 
 abstract class Adapter
 {
-    protected $curlConnectTimeoutInMilliseconds = 1000;
-    protected $curlDownloadTimeoutInSeconds = 11;
+    protected $curlConnectTimeoutInMilliseconds = 1500;
+    protected $curlDownloadTimeoutInSeconds = 50;
 
     abstract public function execute($url, $data = null);
 }
